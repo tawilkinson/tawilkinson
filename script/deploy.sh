2658bed947d0adc17568a3ceaa569e52426e5183
Tom Wilkinson <tom@tawilkinson.com>
2019-06-06 21:24:44 +0100

    Passphrase fix

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,6 @@
 eval "$(ssh-agent -s)" # Start the ssh agent
 echo "$DEPLOY_KEY" > deploy_key.pem
 chmod 600 deploy_key.pem # This key should have push access
-ssh-add deploy_key.pem
+sshpass -p "" ssh-add deploy_key.pem
 git remote add deploy $REPO_URI
 git push deploy master
