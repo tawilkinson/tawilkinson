0ea8b8f752fa7906ceed77fcfda9b6c2fd4d8bed
Tom Wilkinson <tom@tawilkinson.com>
2019-06-06 21:15:18 +0100

    Git deployment test

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-then
-    ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf tawilkinson.com
-    mkdir tawilkinson.com
-    quit
-EOF
-
-    cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" tawilkinson.com .
-fi
+eval "$(ssh-agent -s)" # Start the ssh agent
+echo "$DEPLOY_KEY" > deploy_key.pem
+chmod 600 deploy_key.pem # This key should have push access
+ssh-add deploy_key.pem
+git remote add deploy $REPO_URI
+git push deploy master
