2e67e677ed69dd6a90cf095fdf6d305465151ad9
Tom Wilkinson <tom@tawilkinson.com>
2019-06-06 23:25:04 +0100

    Use pub ssh key

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start the ssh agent
-echo "$DEPLOY_KEY" > deploy_key.pem
-chmod 600 deploy_key.pem # This key should have push access
-ssh-add deploy_key.pem
+echo "$DEPLOY_KEY" > deploy_key.pub
+chmod 600 deploy_key.pub # This key should have push access
+sshpass -p "" ssh-add deploy_key.pub
 git remote add deploy $REPO_URI
 git push deploy master
