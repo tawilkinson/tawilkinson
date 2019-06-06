dba4ff3e8c3236bf781830756517f982c5838282
Tom Wilkinson <tom@tawilkinson.com>
2019-06-07 00:25:42 +0100

    Fix ssh key

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start the ssh agent
-echo "$DEPLOY_KEY" > deploy_key.pub
-chmod 600 deploy_key.pub # This key should have push access
-sshpass -p "" ssh-add deploy_key.pub
+echo "$DEPLOY_KEY" > ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa.pub # This key should have push access
 git remote add deploy $REPO_URI
 git push deploy master
 kill $SSH_AGENT_PID
