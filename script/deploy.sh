3923dcbc8caa3f1c8c05dbd96bc49799fa079900
Tom Wilkinson <tom@tawilkinson.com>
2019-06-06 23:20:46 +0100

    Ignore pass

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,6 @@
 eval "$(ssh-agent -s)" # Start the ssh agent
 echo "$DEPLOY_KEY" > deploy_key.pem
 chmod 600 deploy_key.pem # This key should have push access
-sshpass -p "" ssh-add deploy_key.pem
+ssh-add deploy_key.pem
 git remote add deploy $REPO_URI
 git push deploy master
