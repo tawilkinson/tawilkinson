5511266d643e8311f74cf03edc62a2898ce1aa46
Tom Wilkinson <tom@tawilkinson.com>
2019-06-07 01:34:03 +0100

    Revert "Simpler ssh test"
    This reverts commit ed4129e6274e979eb359207090942a5232b94abf.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start the ssh agent
+mkdir ~/.ssh
 echo "$DEPLOY_KEY" > ~/.ssh/id_rsa.pub
-#chmod 600 ~/.ssh/id_rsa.pub # This key should have push access
+chmod 600 ~/.ssh/id_rsa.pub # This key should have push access
 git remote add deploy $REPO_URI
 git push deploy master
