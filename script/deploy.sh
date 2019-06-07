a4444cebbb6f65b708626d5aee8802802967a31b
Tom Wilkinson <tom@tawilkinson.com>
2019-06-07 10:23:37 +0100

    Add sshpass back in

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,5 +4,6 @@ eval "$(ssh-agent -s)" # Start the ssh agent
 echo "$DEPLOY_KEY" > deploy_key
 chmod 600 deploy_key # This key should have push access
 mv deploy_key ~/.ssh/id_rsa
+sshpass -p "" ~/.ssh/id_rsa
 git remote add deploy $REPO_URI
 git push deploy master
