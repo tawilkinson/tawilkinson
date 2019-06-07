07f8791cb8f7288d40a60ed8bd7c56b039f117d8
Tom Wilkinson <tom@tawilkinson.com>
2019-06-07 23:37:16 +0100

    Learn Travis ssh fix
    https://github.com/dwyl/learn-travis/blob/master/encrypted-ssh-keys-deployment.md

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start the ssh agent
-echo "$DEPLOY_KEY" > deploy_key
-chmod 600 deploy_key # This key should have push access
-mv deploy_key ~/.ssh/id_rsa
-sshpass -p "" ~/.ssh/id_rsa
+chmod 600 ./deploy_key # This key should have push access
+ssh-add ./deploy_key
 git remote add deploy $REPO_URI
 git push deploy master
