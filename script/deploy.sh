036866a5a5fc41a03ed2f4624d1fe48fe7eeb376
Tom Wilkinson <tom@tawilkinson.com>
2019-06-07 01:02:15 +0100

    Use .travis.yml for ssh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-eval "$(ssh-agent -s)" # Start the ssh agent
-echo "$DEPLOY_KEY" > ~/.ssh/id_rsa.pub
-#chmod 600 ~/.ssh/id_rsa.pub # This key should have push access
 git remote add deploy $REPO_URI
 git push deploy master
