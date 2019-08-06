f8b9cfc13472b49f6679c4efa81febbc161c4b9e
Tom Wilkinson <tom.wilkinson@mrsolutions.com>
2019-08-06 12:28:47 +0100

    Cleanup travis.yml

--- script/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-eval "$(ssh-agent -s)" # Start the ssh agent
-chmod 600 ./deploy_key # This key should have push access
-ssh-add ./deploy_key
-git remote add deploy $REPO_URI
-git push deploy master
