3f219ae7a927a8e1d27f3638e2aa4bc77ea10ec3
twilkinson <tom.wilkinson@mrsolutions.com>
2021-05-07 16:25:34 +0100

    Don't delete .well-known

--- script/ci-deploy.sh
+++ script/ci-deploy.sh
@@ -7,4 +7,4 @@ mkdir $1/tmp
 tar -xzvf $1/tar/build.tar.gz -C $1/tmp
 
 # overwrite the content of the /site dir with the /tmp dir
-rsync -Pav --delete $1/tmp/ $1/public_html/
+rsync -Pav --delete $1/tmp/ $1/public_html/ --exclude=$1/public_html/.well-known
